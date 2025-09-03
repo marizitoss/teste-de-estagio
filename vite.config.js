83ed61c3483c090f242616a51fafbb3a366b7194
Marina Lyndsay <lyndsaym49@gmail.com>
2025-09-03 14:36:44 -0400

    Update: Set base path for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react'
 import {defineConfig} from 'vite'
 
-const basePath = process.env.VITE_BASE_PATH || '/'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: basePath,
+  base: '/teste-de-estagio/',
 })
